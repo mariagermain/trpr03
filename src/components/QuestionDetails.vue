0cca98fcd2ee0724ec475a78a9fa665da9f822c3
Maria Germain <mariagermain224@gmail.com>
2024-05-02 20:25:06 -0400

    details

---
+++ src/components/QuestionDetails.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+
+const emit = defineEmits(['delete-question'])
+
+const props = defineProps<{
+    isLoading : boolean,
+    student : string,
+    question : string,
+    category  :string,
+    priority : string
+}>()
+
+function deleteSelectedQuestion() {
+    emit('delete-question')
+}
+
+</script>
+
+<template>
+<div class="container border border-dark border-1 rounded w-60">
+        <span class="col w-100">
+            <h2>Détail: </h2>
+            <div>
+                Priorité: {{ props.priority }}
+            </div>
+            <div>
+                Catégorie: {{ props.category }}
+            </div>
+            <div>
+                Question: {{ props.question }}
+            </div>
+            <div>
+                Étudiant: {{ props.student }}
+            </div>
+            <div v-if="isLoading" class="loader m-1"></div>
+            <button v-if="!isLoading" type="button" class="btn btn-outline-danger m-1" @click="deleteSelectedQuestion()" >Supprimer cette question</button>
+        </span>
+    </div>
+</template>
+
+<style>
+
+/*Nous avons trouvé le CSS sur internet*/
+/*https://www.w3schools.com/howto/howto_css_loader.asp*/
+.loader {
+    border: 2px solid #f3f3f3; /* Light grey */
+    border-top: 2px solid blue; /* Blue */
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    animation: spin 2s linear infinite;
+}
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+</style>
--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -2,6 +2,7 @@
 import { type Ref, ref } from 'vue';
 import AppService from '../../AppService';
 import type { Question } from '@/scripts/Types';
+import QuestionDetails from './QuestionDetails.vue'
 
 const APP_SERVICE : AppService = new AppService();
 
@@ -10,6 +11,7 @@ let questions : Ref<Question[]> = ref(await APP_SERVICE.getQuestions());
 let selectedQuestion = ref();
 
 let isLoading : Ref<boolean> = ref(false);
+
 function selectQuestion(question : Question){
     selectedQuestion.value = question;
 }
@@ -25,26 +27,10 @@ async function deleteSelectedQuestion(){
 </script>
 
 <template>
-    <div v-if="selectedQuestion != undefined" class="container border border-dark border-1 rounded w-60">
-        <span class="col w-100">
-            <h2>Détail: </h2>
-            <div>
-                Priorité: {{ selectedQuestion.priority }}
-            </div>
-            <div>
-                Catégorie: {{ selectedQuestion.category }}
-            </div>
-            <div>
-                Question: {{ selectedQuestion.value }}
-            </div>
-            <div>
-                Étudiant: {{ selectedQuestion.studentName }}
-            </div>
-            <div v-if="isLoading" class="loader m-1"></div>
-            <button v-if="!isLoading" type="button" class="btn btn-outline-danger m-1" @click="deleteSelectedQuestion()" >Supprimer cette question</button>
-        </span>
-    </div>
-
+    <QuestionDetails v-if="selectedQuestion != undefined" @delete-question="deleteSelectedQuestion" :isLoading="isLoading"
+        :student="selectedQuestion.studentName" :question="selectedQuestion.value" 
+        :category="selectedQuestion.category" :priority="selectedQuestion.priority"/>
+    
     <div class="container border border-dark border-1 rounded w-60 mt-2">
         <span class="col w-100">
             <h2>Liste des questions</h2>
@@ -59,21 +45,6 @@ async function deleteSelectedQuestion(){
 </template>
 
 <style scoped>
-
-/*Nous avons trouvé le CSS sur internet*/
-/*https://www.w3schools.com/howto/howto_css_loader.asp*/
-.loader {
-    border: 2px solid #f3f3f3; /* Light grey */
-    border-top: 2px solid blue; /* Blue */
-    border-radius: 50%;
-    width: 20px;
-    height: 20px;
-    animation: spin 2s linear infinite;
-}
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
 
 .P1{
     height: 10px;
