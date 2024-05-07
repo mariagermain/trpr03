fa313b5367967deda0883809052ec81d89cf304e
TheoHautois <theo.hautois@gmail.com>
2024-05-07 12:30:20 -0400

    Ajout d'une page pour modifier les options du compte

---
+++ src/components/AccountEditForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useProfileStore } from '@/stores/profileStore';
+
+const profileStore = useProfileStore()
+let name = profileStore.name
+</script>
+
+<template>
+    <label for="name">Nouveau nom :</label>
+    <input type="text" name="name" id="name" :value="name"/>
+    
+    <label for="new-password">Nouveau mot de passe :</label>
+    <input type="password" name="new-password" id="new-password">
+
+    <label for="confirm-password">Confirmer le mot de passe :</label>
+    <input type="password" name="confirm-password" id="confirm-password">
+
+    <button class="btn btn-primary">Confirmer</button>
+    <button class="btn btn-danger">Annuler</button>
+</template>
+
+<stype scoped>
+
+</stype>
---
+++ src/views/AccountInfo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import { useProfileStore } from '@/stores/profileStore';
+    import AccountEditForm from '@/components/AccountEditForm.vue';
+
+</script>
+
+<template>
+    <AccountEditForm/>
+</template>
+
+<style>
+</style>
